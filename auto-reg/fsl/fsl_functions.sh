33eec81ae2d643680c7f7045f32ecc3b0a7dc8bb
Tom Riis <tomriis@toms-mbp.uguest.utah.edu>
2018-06-05 13:31:10 -0600

    rest of fsl functions+parameters

--- auto-reg/fsl/fsl_functions.sh
+++ auto-reg/fsl/fsl_functions.sh
@@ -8,5 +8,9 @@ function execute_fast(){
     fast -t $1 -n $2 -H $3 -I $4 -l $5 -o $6 $7
 }
 
+function execute_flirt(){
+    flirt -in $1 -ref $2 -out $3 -omat $4 -bins $5 -cost $6 -searchrx $7 -searchry $8 -searchrz $9 -dof $10 -interp $11
+}
+
 
 

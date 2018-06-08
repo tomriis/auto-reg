ca7b782b9d33ce57b4bf55d06c803f2b8be83b78
Tom Riis <tomriis11@gmail.com>
2018-06-08 11:41:45 -0600

    Update auto-reg.py

--- auto-reg/auto-reg.py
+++ auto-reg/auto-reg.py
@@ -1,5 +1,7 @@
 #auto-reg.py
-""" User interface for running all software pipelines on two files"""
+""" User interface for running all software platforms"""
+import platforms
+
 import Tkinter
 from Tkconstants import *
 tk = Tkinter.Tk()

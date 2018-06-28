cacebfc1ba3d3024320a2a4f9f467ec62b18dc6d
Tom Riis <tomriis@toms-mbp.uguest.utah.edu>
2018-06-28 10:04:47 -0600

    pipeline structure

--- auto-reg/auto-reg.py
+++ auto-reg/auto-reg.py
@@ -3,3 +3,25 @@
 import Tkinter
 from Tkconstants import *
 tk = Tkinter.Tk()
+
+import platforms
+import img_pipe
+import utils
+
+class Pipeline(object):
+    def __init__(self,subj):
+        self.subj = subj
+        self.patient = img_pipe.freeCoG(subj=self.subj, hem = 'stereo')
+    def preprocess(self):
+        self.patient.prep_recon()
+        self.patient.get_recon()
+        self.patient.convert_fsmesh2mlab()
+        self.patient.get_subcort()
+
+    def coregister(self, methods = []):
+        if len(methods) == 0:
+            methods = ['ants','spm','fsl','img_pipe']
+        for method in methods:
+            
+
+    def evaluate(self):
